4ad1d9f8d0066c3866590071eae38d222f8f7633
shockValue666 <mapeDeveloper@protonmail.com>
2022-12-23 23:21:12 +0200

    fdaf

--- index.js
+++ index.js
@@ -36,11 +36,11 @@ app.get("/:id",async (req,res)=>{
   // console.log("results: ",body)
 
   const userId = req.params.id;
-  console.log("userid: ",userId)
+  // console.log("userid: ",userId)
   const guildMemberURL = `https://discord.com/api/guilds/${guildId}/members/${userId}`;
-  console.log("guildMemberURL: ",guildMemberURL)
+  // console.log("guildMemberURL: ",guildMemberURL)
   const mem = await request(guildMemberURL,{method:"GET",headers})
-  console.log("mem: ",mem)
+  // console.log("mem: ",mem)
   const members = await mem.body.json()
   console.log("members: ",members)
   if(members.roles){
@@ -56,9 +56,90 @@ app.get("/:id",async (req,res)=>{
   res.json(hasRole)
 })
 
+
+app.get("/isInServer/:id",async (req,res)=>{
+  let isInServer=false;
+  const userId = req.params.id;
+  const specimenId = process.env.specimenId;
+  const guildMemberURL = `https://discord.com/api/guilds/${guildId}/members/${userId}`;
+  // console.log("guildMemberURL: ",guildMemberURL)
+  const mem = await request(guildMemberURL,{method:"GET",headers})
+  // console.log("mem: ",mem)
+  const members = await mem.body.json()
+  console.log("members: ",members)
+
+  if(members.roles){
+    members.roles.map(memberRole=>{
+      console.log("memberRole: ",memberRole, " specimenId: ", specimenId)
+      if(memberRole==specimenId){
+        isInServer=true;
+        // res.json(hasRole)
+      }
+    })
+  }
+
+  res.json(isInServer)
+
+
+})
+
+
+app.get("/addRole/:id",async (req,res)=>{
+  let wlId = process.env.wlId
+  let hasRole;
+
+
+  const moreHeaders = {
+    authorization: `Bot ${bot_token}`,
+     "Content-Type": "application/json",
+  };
+
+
+  //wl id = id of lablisted
+
+  //id of test role = 1054914485464350740
+  const userId = req.params.id;
+  console.log("userid: ",userId)
+  const url = `https://discord.com/api/guilds/978311819384987688/members/${userId}/roles/1054914485464350740`;
+    const options = {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bot ${bot_token}`,
+        'Content-Type': 'application/json',
+      }
+    };
+
+    request(url, options)
+      .then((_res) => {
+        if (_res.statusCode == 204) {
+          console.log(`${userId}: ✔`);
+          res.json(true)
+          return true;
+        } else {
+          console.log(`${userId}: ✘`);
+          console.log("res: ",res.body)
+          res.json(false)
+          return false;
+        }
+      })
+      .catch((error) => {
+        console.log(`${userId}: ✘`);
+        console.error("error: ",error);
+        res.json(false)
+        return false;
+      });
+
+      console.log("hasrole: ",hasRole)
+})
+
+
+
 app.get("/",(req,res)=>{
   res.json(false);
 })
+
+
+
 
 app.listen(port,()=>{
   console.log("listening on port: ",port)
